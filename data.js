e9a3c4435fee74de9933b32fd566ef2e83f8a446
kiet019 <kietly019@gmail.com>
2023-06-09 15:07:30 +0700

    update

---
+++ data.js
@@ -0,0 +1,31 @@
+var setup = {
+    optionsFrom: [
+        "Vietnam",
+        "option 2",
+        "option 3"
+    ],
+    optionsTo: [
+        "Vietnam",
+        "option 2",
+        "option 3"
+    ],
+    QandA: [
+        {
+          question: "Question 1",
+          answer: "Others in our industry calculate shipping prices based on volumetric weight (using the weight and size of the package), which can lead to unexpected charges after it ships to you. MyUS guaranteed international shipping rates are based only on package weight, so there are no surprises.",
+        },
+        {
+          question: "Question 2",
+          answer: "Answer 2",
+        },
+        {
+          question: "Question 3",
+          answer: "Answer 3",
+        },
+        {
+          question: "Question 4",
+          answer: "Answer 4",
+        },
+      ]
+}
+window.setup = setup;
